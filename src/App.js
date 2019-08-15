0fa9c6340558b67af5681c6d8a83cfb2cb45afcb
fdemian <federicodamiancaminiti@gmail.com>
2019-08-15 16:16:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import GreetingFetcher from './GreetingFetcher';
 
-const client = new ApolloClient(/*{uri: 'https://48p1r2roz4.sse.codesandbox.io',}*/);
+const client = new ApolloClient({uri: 'https://path/to/graphqlserver'});
 
 const App = () => (
   <ApolloProvider client={client}>
