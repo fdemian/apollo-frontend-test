423842e0f740f7197abeb7186fc5b23cdf2f44ac
fdemian <demian.fc@gmail.com>
2019-08-16 01:41:55 -0300

    Add exchange rates URI.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import GreetingFetcher from './GreetingFetcher';
+//import ExchangeRates from './ExchangeRates';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  /*uri: 'https://48p1r2roz4.sse.codesandbox.io'*/
+});
 
 const App = () => (
   <ApolloProvider client={client}>
